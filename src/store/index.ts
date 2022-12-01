02c6aafff0357a6e23990026629ffea536400aa8
forellko <dragonforce97q@gmail.com>
2022-12-01 18:43:32 +0300

    Create mutation in store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,15 @@
+import { githubReducer } from './github/github.slice';
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { githubApi } from "./github/github.api";
 
 export const store = configureStore({
   reducer: {
-    [githubApi.reducerPath]: githubApi.reducer
+    [githubApi.reducerPath]: githubApi.reducer,
+    github: githubReducer 
   },
   middleware: (mw) => {
     return mw().concat(githubApi.middleware)}
 })
+
+export type RootState = ReturnType<typeof store.getState>
