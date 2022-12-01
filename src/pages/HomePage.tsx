e80e9faf751aed20edad35e1db478d8b70d12b84
forellko <dragonforce97q@gmail.com>
2022-12-01 14:59:05 +0300

    Hide list on 0 elements

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,13 +4,14 @@ import { useSearchUsersQuery } from '../store/github/github.api';
 
 export default function HomePage() {
   const [search, setSearch] = useState('');
+  const [dropdown, setDropdown] = useState(false);
   const debounced = useDebounce(search);
 
   const { isLoading, isError, data } = useSearchUsersQuery(debounced, {
     skip: debounced.length < 3,
   });
   useEffect(() => {
-    console.log(debounced);
+    setDropdown(debounced.length > 3 && data?.length! > 0);
   }, [debounced]);
 
   return (
@@ -27,17 +28,19 @@ export default function HomePage() {
           onChange={(e) => setSearch(e.target.value)}
         />
 
-        <ul className="absolute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white">
-          {isLoading && <p className="text-center">Loading</p>}
-          {data?.map((user) => (
-            <li
-              key={user.id}
-              className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
-            >
-              {user.login}
-            </li>
-          ))}
-        </ul>
+        {dropdown && (
+          <ul className="absolute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white">
+            {isLoading && <p className="text-center">Loading</p>}
+            {data?.map((user) => (
+              <li
+                key={user.id}
+                className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
+              >
+                {user.login}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
