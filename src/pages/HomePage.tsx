89fcc88511be5ba6fb77177404ccd92def4e32ac
forellko <dragonforce97q@gmail.com>
2022-12-01 13:41:39 +0300

    Add RTK Query. Get user from github. Add middleware! to store

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useSearchUsersQuery } from '../store/github/github.api';
 
 export default function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+  const { isLoading, isError, data } = useSearchUsersQuery('forellko');
+  return <div>HomePage</div>;
 }
---
+++ src/store/index.ts
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { githubApi } from "./github/github.api";
+
+export const store = configureStore({
+  reducer: {
+    [githubApi.reducerPath]: githubApi.reducer
+  },
+  middleware: (mw) => {
+    return mw().concat(githubApi.middleware)}
+})
